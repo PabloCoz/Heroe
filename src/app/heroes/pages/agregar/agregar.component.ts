9fd385dadfaf791b38a735b988fd4a3f2196d81a
Pablo Coz <pablocr_234@hotmail.com>
2023-04-14 18:00:26 -0500

    segundo commit

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.css']
 })
-export class AgregarComponent {
+export class AgregarComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    //Recibir el id del heroe
+    this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
+    })
+  }
 
 }
--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit {
+
+  heroe!: Heroe
+  constructor(private activatedRoute: ActivatedRoute, private heoresService: HeroesService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe(
+      switchMap(({ id }) => this.heoresService.getHeroePorId(id))
+    ).subscribe(heroe => this.heroe = heroe)
+  }
 
 }
